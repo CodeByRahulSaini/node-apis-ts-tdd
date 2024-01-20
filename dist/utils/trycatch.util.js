530053600ec22062221a5cc1db730fc12756fdbe
Rahul Saini <om.ks.rs@gmail.com>
2024-01-21 01:07:49 +0530

    update dotenv, update gitignore

--- dist/utils/trycatch.util.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tryCatch = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch((err) => {
-        next(err);
-    });
-};
-exports.default = tryCatch;
